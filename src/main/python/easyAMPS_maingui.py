ffb30bc2c9d1bf7c6f5ea7a207f8d97928f0ee95
daniel.elnatan@biodesy.com <delnatan@Daniels-MacBook-Pro-2.local>
2020-05-06 18:28:25 -0700

    updated GUI tab to include 4-ch prediction

--- src/main/python/easyAMPS_maingui.py
+++ src/main/python/easyAMPS_maingui.py
@@ -358,7 +358,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuTools.menuAction())
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(3)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
