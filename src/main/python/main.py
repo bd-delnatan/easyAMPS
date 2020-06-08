59131e1c2ab8bebd92ba3b2f4ce96e376b4fb00a
daniel.elnatan@biodesy.com <daniel.elnatan@biodesy.com>
2020-06-08 15:03:30 -0700

    updated to version 0.15b

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -23,7 +23,7 @@ from AMPS import _AMPSboundaries
 from easyAMPS_maingui import Ui_MainWindow
 from DebugWindow import ScriptWindow
 
-__VERSION__ = "0.1.5a"
+__VERSION__ = "0.1.5b"
 
 
 # needed to properly scale high DPI screens in Windows OS
